429820c710f6f4a814df8e67273ab4efa2f48e3b
Julianne <juliannecsetiadi@email.arizona.edu>
2022-11-08 17:10:55 -0700

    Test commit

--- lambda/index.js
+++ lambda/index.js
@@ -52,7 +52,7 @@ const ActivateSurveyIntentHandler = {
 
   handle(handlerInput) {
     const speakOutput =
-      'Welcome to the Antaris health survey built by 23062 team. Edit from Khaled and Darianne and Son and Wesley computer!';
+      'Welcome to the Antaris health survey built by 23062 team. Edit from Khaled and Darianne and Son and Wesley fhfhcomputer!';
 
     return (
       handlerInput.responseBuilder
