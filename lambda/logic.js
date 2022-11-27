cf60fbdd5194ee900ab329fb82b9923a4a0218b9
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-11-27 08:56:34 +0000

    updated lambda/logic.js.

---
+++ lambda/logic.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+
+const hostname = 'http://localhost:3000';
+
+module.exports = {
+    async fetchParticipantInfo(participantID) {
+        const apiRoute = `${hostname}/api/participants/${participantID}?fields=antaris_id,name`;
+        const response = await axios.get(apiRoute);
+        
+        const config = {
+            timeout: 6500
+        }
+
+        try {
+            const response = await axios.get(apiRoute, config);
+            return  response.data;
+        } 
+        catch (error) {
+            console.log('ERROR', error);
+            return null;
+        }
+    }
+}
