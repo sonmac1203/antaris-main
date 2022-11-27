b694bbd1e02ccf97afeadc9c52a7f8fce321ec46
Son Mac <mactranthienson@gmail.com>
2022-11-27 02:29:22 -0700

    Update fetchParticipantInfo

--- lambda/logic.js
+++ lambda/logic.js
@@ -3,20 +3,20 @@ const axios = require('axios');
 const hostname = 'http://localhost:3000';
 
 module.exports = {
-    async fetchParticipantInfo(participantID) {
-        const apiRoute = `${hostname}/api/participants/${participantID}?fields=antaris_id,name`;
-        
-        const config = {
-            timeout: 6500
-        }
+  async fetchParticipantInfo(participantID) {
+    const apiRoute = `${hostname}/api/participants/${participantID}?fields=antaris_id,name`;
 
-        try {
-            const response = await axios.get(apiRoute, config);
-            return  response.data;
-        } 
-        catch (error) {
-            console.log('ERROR', error);
-            return null;
-        }
+    const config = {
+      timeout: 6500,
+    };
+
+    try {
+      const response = await axios.get(apiRoute, config);
+      return response.data;
+    } catch (error) {
+      console.log('ERROR', error);
+      const response = error.response;
+      return response.data;
     }
-}
+  },
+};
