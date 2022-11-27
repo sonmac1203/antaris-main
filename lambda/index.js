fe16d589f9b737d93d2d69470e1caac286e50add
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-11-27 22:07:21 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -119,9 +119,9 @@ const BeginSurveyIntentHandler = {
     const studyID = sessionAttributes.choosenStudyID;
 
     const response = await logic.fetchStudyInfo(studyID);
-    const questions = logic.populateQuestions(response.data.study_data);
-    sessionAttributes.questions = questions;
-    handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
+    // const questions = logic.populateQuestions(response.data.study_data);
+    // sessionAttributes.questions = questions;
+    // handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
 
     const speakOutput =
       `Welcome to the Antaris health survey built by 23062 team. You chose ${studyID} We are Khaled, Darianne, Son, Wesley and Julianne! The first question is ${response.data.study_data['ODM']['$']['xmlns']} Say read the questions to continue.`;
