018df50676cf522a82ffee088289d9e45b5ba4cf
Son Mac <mactranthienson@gmail.com>
2022-11-27 05:06:52 -0700

    Add function to convert list of studies to string

--- lambda/logic.js
+++ lambda/logic.js
@@ -3,21 +3,31 @@ const axios = require('axios');
 const host = 'https://7480-68-104-159-200.ngrok.io';
 
 module.exports = {
-    async fetchParticipantInfo(participantID) {
-        const apiRoute = `${host}/api/participants/${participantID}?fields=antaris_id,name`;
-        
-        const config = {
-            timeout: 6500
-        }
-
-        try {
-            const response = await axios.get(apiRoute, config);
-            return  response.data;
-        } 
-        catch (error) {
-            console.log('ERROR', error);
-            const response = error.response;
-            return response.data;
-        }
+  async fetchParticipantInfo(participantID) {
+    const apiRoute = `${host}/api/participants/${participantID}?fields=antaris_id,name,studies`;
+    const config = {
+      timeout: 6500,
+    };
+    try {
+      const response = await axios.get(apiRoute, config);
+      return response.data;
+    } catch (error) {
+      console.log('ERROR', error);
+      const response = error.response;
+      return response.data;
     }
-}
+  },
+
+  getVerbalStudyList(studies) {
+    const len = studies.length;
+    let speech = '';
+    for (const i = 0; i < len; i++) {
+      if (i !== len - 1) {
+        speech += `, ${studies[i]}`;
+      } else {
+        speech += `and ${studies[i]}.`;
+      }
+    }
+    return speech;
+  },
+};
