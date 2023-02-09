ec8d986922c04be0d6221de006b4cf66e49cf455
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2023-02-09 06:57:04 +0000

    updated lambda/apl.js.

---
+++ lambda/apl.js
@@ -0,0 +1,32 @@
+module.exports = {
+    TEST_APL: {
+        DOCUMENT_ID:"Greeting",
+        datasource: {
+            "headlineTemplateData": {
+                "type": "object",
+                "objectId": "headlineSample",
+                "properties": {
+                    "backgroundImage": {
+                        "contentDescription": null,
+                        "smallSourceUrl": null,
+                        "largeSourceUrl": null,
+                        "sources": [
+                            {
+                                "url": "https://d2o906d8ln7ui1.cloudfront.net/images/templates_v3/headline/HeadlineBackground_Dark.png",
+                                "size": "large"
+                            }
+                        ]
+                    },
+                    "textContent": {
+                        "primaryText": {
+                            "type": "PlainText",
+                            "text": "Welcome to Antaris Health survey"
+                        }
+                    },
+                    "logoUrl": "",
+                    "hintText": "Try, \"Alexa, start my fantastic health survey\""
+                }
+            }
+        },
+    }
+}
