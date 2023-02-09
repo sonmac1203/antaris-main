623450b52654bffdb6b21eb584a6e0bc23cce3ce
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2023-02-09 22:19:38 +0000

    updated lambda/apl.js.

--- lambda/apl.js
+++ lambda/apl.js
@@ -63,45 +63,44 @@ module.exports = {
         DOCUMENT_ID: "TestStudyList",
         datasource: {
             "textListData": {
-                "type": "object",
-                "objectId": "textListSample",
-                "id": "textListSample",
-                "backgroundImage": {
-                    "contentDescription": null,
-                    "smallSourceUrl": null,
-                    "largeSourceUrl": null,
-                    "sources": [
-                        {
-                            "url": "https://d2o906d8ln7ui1.cloudfront.net/images/templates_v3/textlist/AlexaTextListBackground_Dark.png",
-                            "size": "large"
-                        }
-                    ]
-                },
-                "title": "Hi Wesley",
-                "primaryAction": {
-                  "type": "SendEvent",
-                  "arguments": [
-                    "ListItemSelected",
-                    "${ordinal}"
-                  ]
-                },
-                "listItems": [
+                "headerTitle": "Alexa text list header title",
+                "headerSubtitle": "Header subtitle",
+                "headerAttributionImage": "https://s3.amazonaws.com/ask-skills-assets/apl-layout-assets/attribution_dark_hub_prime.png",
+                "backgroundImageSource": "https://d2o906d8ln7ui1.cloudfront.net/images/BT6_Background.png",
+                "listItemsToShow": [
                     {
                         "primaryText": "First item primary text",
                         "secondaryText": "This is the secondary text",
                         "secondaryTextPosition": "bottom",
-                        "touchForward": true,
-                        "id": "studyItem",
+                        "tertiaryText": "This is the tertiary text",
+                        "tertiaryTextPosition": "bottom",
+                        "ratingNumber": 2,
+                        "imageThumbnailSource": "https://d2o906d8ln7ui1.cloudfront.net/images/md_brie.png",
+                        "hideOrdinal": true,
+                        "touchForward": true
                     },
                     {
-                        "primaryText": "Ivy Lane Nursery and Tree Farm",
-                        "secondaryText": "This is the secondary text",
-                        "secondaryTextPosition": "bottom",
-                        "touchForward": true,
-                        "id": "studyItem",
+                        "primaryText": "Second item in the list.",
+                        "secondaryText": "Secondary text for the second item",
+                        "imageThumbnailSource": "https://d2o906d8ln7ui1.cloudfront.net/images/md_gruyere.png",
+                        "tertiaryText": "Tertiary text",
+                        "tertiaryTextPosition": "bottom",
+                        "ratingNumber": 0
+                    },
+                    {
+                        "primaryText": "Third item in the list.",
+                        "imageThumbnailSource": "https://d2o906d8ln7ui1.cloudfront.net/images/sm_blue.png"
+                    },
+                    {
+                        "primaryText": "Fourth item in the list"
+                    },
+                    {
+                        "primaryText": "Fifth item in the list"
+                    },
+                    {
+                        "primaryText": "This list might have many more items"
                     }
-                ],
-                "logoUrl": "https://d2o906d8ln7ui1.cloudfront.net/images/templates_v3/logo/logo-modern-botanical-white.png",
+                ]
             }
         },
     }
