13a3521f1e266e2b9769cd1bce121fc85231c902
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2023-02-21 23:03:54 +0000

    updated lambda/logic.js.

--- lambda/logic.js
+++ lambda/logic.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const utils = require('./util');
 const zlib = require('zlib');
 
-const host = 'https://306a-172-58-19-182.ngrok.io';
+const host = 'https://006f-172-58-19-182.ngrok.io';
 
 module.exports = {
     async fetchParticipantInfo(participantID) {
