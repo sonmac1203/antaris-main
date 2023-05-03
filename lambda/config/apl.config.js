6d80f955458ec41daff8d2d475bbcf83a132a747
Son Mac <mactranthienson@gmail.com>
2023-05-02 18:58:05 -0700

    Add apl.config and apl.utils

---
+++ lambda/config/apl.config.js
@@ -0,0 +1,16 @@
+const LOGO_URL =
+    'https://drive.google.com/uc?id=1pHAgpzA_vlhZa291LLjlvO9R--0nhbQI';
+const BG_URL =
+    'https://d2o906d8ln7ui1.cloudfront.net/images/templates_v3/headline/HeadlineBackground_Dark.png';
+const SECONDARY_BG_URL =
+    'https://drive.google.com/uc?id=1iO9wfcV_NfJvosJtUEEVQ30HLqQi2WrA';
+const HEADER_TITLE = 'ENGR 498 CAPSTONE PROJECT - TEAM 23062';
+const QUESTION_HINT_TEXT = 'Always start with "My answer is ..."';
+
+module.exports = {
+    LOGO_URL,
+    BG_URL,
+    SECONDARY_BG_URL,
+    HEADER_TITLE,
+    QUESTION_HINT_TEXT,
+};
---
+++ lambda/utils/apl.utils.js
@@ -0,0 +1,160 @@
+const {
+    BG_URL,
+    SECONDARY_BG_URL,
+    HEADER_TITLE,
+    LOGO_URL,
+    QUESTION_HINT_TEXT,
+} = require('../config/apl.config');
+
+module.exports = {
+    createDirectivePayload(
+        aplDocumentId,
+        dataSources = {},
+        tokenId = 'documentToken'
+    ) {
+        return {
+            type: 'Alexa.Presentation.APL.RenderDocument',
+            token: tokenId,
+            document: {
+                type: 'Link',
+                src: 'doc://alexa/apl/documents/' + aplDocumentId,
+            },
+            datasources: dataSources,
+        };
+    },
+    getBasicAnnouncementAplDirective(text, hintText) {
+        const DOCUMENT_ID = 'BasicAnnouncement';
+        const dataSources = {
+            templateData: {
+                type: 'object',
+                objectId: 'headline',
+                properties: {
+                    backgroundImage: {
+                        contentDescription: null,
+                        smallSourceUrl: null,
+                        largeSourceUrl: null,
+                        sources: [
+                            {
+                                url: BG_URL,
+                                size: 'large',
+                            },
+                        ],
+                    },
+                    textContent: {
+                        primaryText: {
+                            type: 'PlainText',
+                            text: text,
+                        },
+                        headerTitle: {
+                            type: 'PlainText',
+                            text: HEADER_TITLE,
+                        },
+                    },
+                    logoUrl: LOGO_URL,
+                    hintText: hintText,
+                },
+            },
+        };
+        const payload = this.createDirectivePayload(
+            DOCUMENT_ID,
+            dataSources,
+            'documentToken'
+        );
+        return payload;
+    },
+
+    getBasicQuestionAplDirective(question, surveyName) {
+        const { questionNumber, questionText } = question;
+        const DOCUMENT_ID = 'QuestionDisplay';
+        const dataSources = {
+            questionData: {
+                type: 'object',
+                objectId: 'questionDisplay',
+                properties: {
+                    backgroundImage: {
+                        contentDescription: null,
+                        smallSourceUrl: null,
+                        largeSourceUrl: null,
+                        sources: [
+                            {
+                                url: SECONDARY_BG_URL,
+                                size: 'large',
+                            },
+                        ],
+                    },
+                    headerContent: {
+                        questionNumber,
+                        surveyName,
+                    },
+                    textContent: {
+                        primaryText: {
+                            type: 'PlainText',
+                            text: questionText,
+                        },
+                    },
+                    logoUrl: LOGO_URL,
+                    hintText: QUESTION_HINT_TEXT,
+                },
+            },
+        };
+
+        const payload = this.createDirectivePayload(
+            DOCUMENT_ID,
+            dataSources,
+            'documentToken'
+        );
+        return payload;
+    },
+    getSurveyListAplDirective(surveys, participantName) {
+        const surveyNumber = surveys.length;
+        const plural = surveyNumber > 1 ? 's' : '';
+        const surveyNumberInText = `${surveyNumber} survey${plural}`;
+
+        const listItems = surveys.map((s) => ({
+            primaryText: s.name,
+        }));
+
+        const DOCUMENT_ID = 'SurveySelection';
+
+        const dataSources = {
+            surveyListData: {
+                type: 'object',
+                objectId: 'SurveySelectionList',
+                backgroundImage: {
+                    contentDescription: null,
+                    smallSourceUrl: null,
+                    largeSourceUrl: null,
+                    sources: [
+                        {
+                            url: 'https://drive.google.com/uc?id=1Lxpj5z1TxXJcQO_JprXBBfxasp0Kzf8_',
+                            size: 'large',
+                        },
+                    ],
+                },
+                headerContent: {
+                    participantName: participantName,
+                    surveyNumber: surveyNumberInText,
+                },
+                listItems: listItems,
+                logoUrl: LOGO_URL,
+            },
+        };
+
+        const payload = this.createDirectivePayload(
+            DOCUMENT_ID,
+            dataSources,
+            'documentToken'
+        );
+
+        // const aplDirective = {
+        //     type: 'Alexa.Presentation.APL.RenderDocument',
+        //     token: 'documentToken',
+        //     document: {
+        //         type: 'Link',
+        //         src: 'doc://alexa/apl/documents/SurveySelection',
+        //     },
+        //     datasources: dataSources,
+        // };
+        return payload;
+    },
+};
